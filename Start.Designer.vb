35020e52331fb469463b456268cc5f614cbed8d5
Julien Poirot <julien.poirot@topsolid.com>
2023-12-16 08:59:09 +0100

    Tout OK sauf Chemin

---
+++ Start.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Start
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBoxProjectName = New System.Windows.Forms.TextBox()
+        Me.TreeViewProjectFolders = New System.Windows.Forms.TreeView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CheckBoxRecursiveSelection = New System.Windows.Forms.CheckBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.FolderBrowserDialogCSVDestination = New System.Windows.Forms.FolderBrowserDialog()
+        Me.TextBoxSelectedCSVDestinationFolder = New System.Windows.Forms.TextBox()
+        Me.FolderBrowserDialog = New System.Windows.Forms.Button()
+        Me.ButtonExecute = New System.Windows.Forms.Button()
+        Me.CheckedListBoxPropertyToExport = New System.Windows.Forms.CheckedListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBoxCSVName = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBoxProjectName
+        '
+        Me.TextBoxProjectName.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBoxProjectName.Location = New System.Drawing.Point(322, 30)
+        Me.TextBoxProjectName.Name = "TextBoxProjectName"
+        Me.TextBoxProjectName.ReadOnly = True
+        Me.TextBoxProjectName.Size = New System.Drawing.Size(445, 20)
+        Me.TextBoxProjectName.TabIndex = 2
+        '
+        'TreeViewProjectFolders
+        '
+        Me.TreeViewProjectFolders.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.TreeViewProjectFolders.CheckBoxes = True
+        Me.TreeViewProjectFolders.Location = New System.Drawing.Point(12, 12)
+        Me.TreeViewProjectFolders.Name = "TreeViewProjectFolders"
+        Me.TreeViewProjectFolders.Size = New System.Drawing.Size(300, 399)
+        Me.TreeViewProjectFolders.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(319, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(97, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Nom projet courant"
+        '
+        'CheckBoxRecursiveSelection
+        '
+        Me.CheckBoxRecursiveSelection.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.CheckBoxRecursiveSelection.AutoSize = True
+        Me.CheckBoxRecursiveSelection.Checked = True
+        Me.CheckBoxRecursiveSelection.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBoxRecursiveSelection.Location = New System.Drawing.Point(12, 417)
+        Me.CheckBoxRecursiveSelection.Name = "CheckBoxRecursiveSelection"
+        Me.CheckBoxRecursiveSelection.Size = New System.Drawing.Size(116, 17)
+        Me.CheckBoxRecursiveSelection.TabIndex = 9
+        Me.CheckBoxRecursiveSelection.Text = "Sélection récursive"
+        Me.CheckBoxRecursiveSelection.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(319, 53)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(111, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Chemin de destination"
+        '
+        'TextBoxSelectedCSVDestinationFolder
+        '
+        Me.TextBoxSelectedCSVDestinationFolder.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBoxSelectedCSVDestinationFolder.Location = New System.Drawing.Point(322, 69)
+        Me.TextBoxSelectedCSVDestinationFolder.Name = "TextBoxSelectedCSVDestinationFolder"
+        Me.TextBoxSelectedCSVDestinationFolder.ReadOnly = True
+        Me.TextBoxSelectedCSVDestinationFolder.Size = New System.Drawing.Size(414, 20)
+        Me.TextBoxSelectedCSVDestinationFolder.TabIndex = 11
+        '
+        'FolderBrowserDialog
+        '
+        Me.FolderBrowserDialog.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FolderBrowserDialog.Location = New System.Drawing.Point(742, 69)
+        Me.FolderBrowserDialog.Name = "FolderBrowserDialog"
+        Me.FolderBrowserDialog.Size = New System.Drawing.Size(25, 20)
+        Me.FolderBrowserDialog.TabIndex = 12
+        Me.FolderBrowserDialog.Text = "..."
+        Me.FolderBrowserDialog.UseVisualStyleBackColor = True
+        '
+        'ButtonExecute
+        '
+        Me.ButtonExecute.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ButtonExecute.Location = New System.Drawing.Point(322, 411)
+        Me.ButtonExecute.Name = "ButtonExecute"
+        Me.ButtonExecute.Size = New System.Drawing.Size(445, 23)
+        Me.ButtonExecute.TabIndex = 13
+        Me.ButtonExecute.Text = "Exécuter Export"
+        Me.ButtonExecute.UseVisualStyleBackColor = True
+        '
+        'CheckedListBoxPropertyToExport
+        '
+        Me.CheckedListBoxPropertyToExport.FormattingEnabled = True
+        Me.CheckedListBoxPropertyToExport.Items.AddRange(New Object() {"Nom", "Désignation", "Référence", "Fabricant", "RéférenceFabricant", "Chemin"})
+        Me.CheckedListBoxPropertyToExport.Location = New System.Drawing.Point(322, 147)
+        Me.CheckedListBoxPropertyToExport.Name = "CheckedListBoxPropertyToExport"
+        Me.CheckedListBoxPropertyToExport.Size = New System.Drawing.Size(445, 94)
+        Me.CheckedListBoxPropertyToExport.TabIndex = 14
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(319, 131)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(99, 13)
+        Me.Label3.TabIndex = 15
+        Me.Label3.Text = "Propriété à exporter"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(319, 92)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(99, 13)
+        Me.Label4.TabIndex = 16
+        Me.Label4.Text = "Nom du fichier CSV"
+        '
+        'TextBoxCSVName
+        '
+        Me.TextBoxCSVName.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBoxCSVName.Location = New System.Drawing.Point(322, 108)
+        Me.TextBoxCSVName.Name = "TextBoxCSVName"
+        Me.TextBoxCSVName.Size = New System.Drawing.Size(445, 20)
+        Me.TextBoxCSVName.TabIndex = 17
+        '
+        'Start
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBoxCSVName)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.CheckedListBoxPropertyToExport)
+        Me.Controls.Add(Me.ButtonExecute)
+        Me.Controls.Add(Me.FolderBrowserDialog)
+        Me.Controls.Add(Me.TextBoxSelectedCSVDestinationFolder)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.CheckBoxRecursiveSelection)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TreeViewProjectFolders)
+        Me.Controls.Add(Me.TextBoxProjectName)
+        Me.Name = "Start"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBoxProjectName As TextBox
+    Friend WithEvents TreeViewProjectFolders As TreeView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CheckBoxRecursiveSelection As CheckBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents FolderBrowserDialogCSVDestination As FolderBrowserDialog
+    Friend WithEvents TextBoxSelectedCSVDestinationFolder As TextBox
+    Friend WithEvents FolderBrowserDialog As Button
+    Friend WithEvents ButtonExecute As Button
+    Friend WithEvents CheckedListBoxPropertyToExport As CheckedListBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBoxCSVName As TextBox
+End Class
---
+++ Start.vb
@@ -0,0 +1,313 @@
+﻿Imports System.IO
+Imports Microsoft.Win32
+
+Public Class Start
+    'Current project
+    Dim CurrentProjectPdmId As PdmObjectId
+    Dim CurrentProjectName As String
+    'Folder
+    Dim SelectedFolderPdmIdList As New List(Of PdmObjectId)
+    Dim SelectedFolderNameList As New List(Of String)
+    Dim FolderIdsToModify As New List(Of PdmObjectId)
+    'CSV
+    Dim SelectedCSVDestinationFolder As String
+    Dim CSVDocumentName As String
+
+    Private Sub Start_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'Connection à TopSolid
+        Try
+            TopSolidHost.Connect()  'Connection à TopSolid
+            TopSolidDesignHost.Connect()    'Connection à TopSolid
+        Catch ex As Exception
+            MsgBox("Impossible de se connecter à TopSolid" + ex.Message)
+            Exit Sub
+        End Try
+
+        Dim TSConnected As Boolean = TopSolidDesignHost.IsConnected
+        If TSConnected = False Then MsgBox("Non connecté")
+
+        Try
+            CurrentProjectPdmId = TopSolidHost.Pdm.GetCurrentProject()   'Récupération ID projet
+        Catch ex As Exception
+            MsgBox("Echec de la récupération de l'id du projet courant" + ex.Message)
+        End Try
+
+        Try
+            CurrentProjectName = TopSolidHost.Pdm.GetName(CurrentProjectPdmId)  'Récupération Nom projet
+            TextBoxProjectName.Text = CurrentProjectName
+        Catch ex As Exception
+            MsgBox("Echec de la récupération de l'id du projet courant" + ex.Message)
+        End Try
+
+        'Récupération de la structure du projet
+        Dim projects As New List(Of PdmObjectId)
+        Dim rootFolderId As PdmObjectId
+        Dim documentIds As New List(Of PdmObjectId)
+        Dim folderIds As New List(Of PdmObjectId)
+        Dim folderDictionary As New Dictionary(Of String, PdmObjectId)
+
+        ' Récupération des sous-dossiers du dossier racine
+        Try
+            TopSolidHost.Pdm.GetConstituents(CurrentProjectPdmId, folderIds, documentIds)
+        Catch ex As Exception
+            MsgBox("Echec récupération des dossiers racine" + ex.Message)
+        End Try
+
+
+        ' Ajout des sous-dossiers du dossier racine au contrôle TreeView
+        Try
+            For Each folderId As PdmObjectId In folderIds
+                Dim folderName As String = TopSolidHost.Pdm.GetName(folderId)
+                'folderDictionary.Add(folderName, folderId) 'Problème déjà élément avec la même clé déjà ajouté
+                Dim folderNode As New TreeNode(folderName)
+                folderNode.Tag = folderId
+                TreeViewProjectFolders.Nodes.Add(folderNode)
+                'AddSubFolders(folderNode, folderId, CurrentProjectPdmId, folderDictionary) 'Problème déjà élément avec la même clé déjà ajouté
+                AddSubFolders(folderNode, folderId, CurrentProjectPdmId) 'Problème déjà élément avec la même clé déjà ajouté
+            Next
+        Catch ex As Exception
+            MsgBox("Echec récupération des sous dossiers" + vbNewLine + ex.Message)
+        End Try
+
+        '
+        SelectedCSVDestinationFolder = GetDesktopFolderPath()
+        TextBoxSelectedCSVDestinationFolder.Text = SelectedCSVDestinationFolder
+
+
+    End Sub
+
+    ' Fonction récursive pour ajouter les sous-dossiers à un noeud de TreeView
+    'Private Sub AddSubFolders(parentNode As TreeNode, parentFolderId As PdmObjectId, ProjectId As PdmObjectId, folderDictionary As Dictionary(Of String, PdmObjectId)) 'Problème déjà élément avec la même clé déjà ajouté
+    Private Sub AddSubFolders(parentNode As TreeNode, parentFolderId As PdmObjectId, ProjectId As PdmObjectId) 'Problème déjà élément avec la même clé déjà ajouté
+        Dim subFolderIds As New List(Of PdmObjectId)
+        Dim subDocumentIds As New List(Of PdmObjectId)
+
+        TopSolidHost.Pdm.GetConstituents(parentFolderId, subFolderIds, subDocumentIds)
+
+        For Each subFolderId As PdmObjectId In subFolderIds
+            Dim subfolderName As String = TopSolidHost.Pdm.GetName(subFolderId)
+            'folderDictionary.Add(subfolderName, subFolderId) 'Problème déjà élément avec la même clé déjà ajouté
+
+            Dim subfolderNode As New TreeNode(subfolderName)
+            subfolderNode.Tag = subFolderId ' Ajout de la propriété Tag pour stocker l'ID du dossier
+            parentNode.Nodes.Add(subfolderNode)
+            'AddSubFolders(subfolderNode, subFolderId, CurrentProjectPdmId, folderDictionary) 'Problème déjà élément avec la même clé déjà ajouté
+            AddSubFolders(subfolderNode, subFolderId, CurrentProjectPdmId)
+        Next
+    End Sub
+
+    Private Sub FolderBrowserDialog_Click(sender As Object, e As EventArgs) Handles FolderBrowserDialog.Click
+
+        'Récupération du chemin du bureau
+        Dim desktopPath As String = GetDesktopFolderPath()
+
+
+        ' Configurer le FolderBrowserDialog
+        With FolderBrowserDialogCSVDestination
+            .Description = "Sélectionner un dossier de destination"
+            ' Définir le dossier de départ (si nécessaire)
+            .SelectedPath = desktopPath
+            .ShowNewFolderButton = True ' Autoriser la création d'un nouveau dossier
+        End With
+
+        ' Afficher le FolderBrowserDialog et vérifier si l'utilisateur a appuyé sur OK
+        If FolderBrowserDialogCSVDestination.ShowDialog() = DialogResult.OK Then
+            ' Récupérer le chemin du dossier sélectionné
+            SelectedCSVDestinationFolder = FolderBrowserDialogCSVDestination.SelectedPath
+            TextBoxSelectedCSVDestinationFolder.Text = SelectedCSVDestinationFolder
+        End If
+    End Sub
+
+    ' Fonction permettant de récupérer le chemin vers le bureau de l'utilisateur via les registres
+    Private Function GetDesktopFolderPath() As String
+        ' Clé du registre pour le chemin vers le bureau
+        Dim registryKeyPath As String = "Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
+        Dim registryValueName As String = "Desktop"
+
+        ' Lire la valeur du registre
+        Try
+            Using registryKey As RegistryKey = Registry.CurrentUser.OpenSubKey(registryKeyPath)
+                If registryKey IsNot Nothing Then
+                    ' Lire la valeur du registre
+                    Dim desktopPath As String = CStr(registryKey.GetValue(registryValueName))
+                    Return desktopPath
+                End If
+            End Using
+        Catch ex As Exception
+            ' Gérer les erreurs liées à la lecture du registre
+            MessageBox.Show($"Erreur lors de la lecture du registre : {ex.Message}")
+        End Try
+
+        ' Retourner une chaîne vide si le chemin n'a pas pu être récupéré
+        Return String.Empty
+    End Function
+
+    'Fonction récursive pour récupérer des sous noeuds
+    Private Sub CheckSubNodes(node As TreeNode)
+
+        If node.Checked Then
+            FolderIdsToModify.Add(DirectCast(node.Tag, PdmObjectId))
+        End If
+        For Each subNode As TreeNode In node.Nodes
+            CheckSubNodes(subNode)
+        Next
+
+    End Sub
+
+
+    ''' <summary>
+    ''' Fonction récursive pour ajouter des sous documents
+    ''' </summary>
+    ''' <param name="parentFolderId">PdmObjectId du dossier parent</param>
+    ''' <param name="documentPdmIds">List des documents à compléter</param>
+    ''' <param name="SubFolder">Recherche ou non les sous dossiers</param>
+    ''' <param name="SubDocument">Recherche ou non les sous documents</param>
+    Private Sub AddSubDocuments(parentFolderId As PdmObjectId, ByRef documentPdmIds As List(Of PdmObjectId), SubFolder As Boolean, SubDocument As Boolean)
+        Dim subFolderIds As New List(Of PdmObjectId)
+        Dim subDocumentIds As New List(Of PdmObjectId)
+
+        TopSolidHost.Pdm.GetConstituents(parentFolderId, subFolderIds, subDocumentIds)
+        documentPdmIds.AddRange(subDocumentIds)
+
+        If SubDocument Then
+            For Each subDocumentId As PdmObjectId In subDocumentIds
+                Dim subDocumentName As String = TopSolidHost.Pdm.GetName(subDocumentId)
+                AddSubDocuments(subDocumentId, documentPdmIds, SubFolder, SubDocument)
+            Next
+        End If
+
+        If SubFolder Then
+            For Each subFolderId As PdmObjectId In subFolderIds
+                AddSubDocuments(subFolderId, documentPdmIds, SubFolder, SubDocument)
+            Next
+
+        End If
+    End Sub
+
+    Private Sub ButtonExecute_Click(sender As Object, e As EventArgs) Handles ButtonExecute.Click
+        For Each node As TreeNode In TreeViewProjectFolders.Nodes ' Récupération de tous les dossiers cochés
+            CheckSubNodes(node)
+        Next
+
+        Dim DocumentIdsToModify As New List(Of DocumentId)
+        Dim DocumentPdmIdsToModify As New List(Of PdmObjectId)
+
+        'Remplacement de la méthode manuel ci après
+        Try
+            For Each FolderIdToModify As PdmObjectId In FolderIdsToModify
+
+                AddSubDocuments(FolderIdToModify, DocumentPdmIdsToModify, CheckBoxRecursiveSelection.Checked, True)
+            Next
+        Catch ex As Exception
+            MsgBox("Echec récupération des Pdm document Ids dans les dossiers sélectionnés" + ex.Message)
+        End Try
+
+        Try
+            For Each DocumentPdmIdToModify In DocumentPdmIdsToModify
+                DocumentIdsToModify.Add(TopSolidHost.Documents.GetDocument(DocumentPdmIdToModify))
+            Next
+        Catch ex As Exception
+            MsgBox("Echec récupération des document Ids dans les dossiers sélectionnés" + ex.Message)
+        End Try
+
+        If DocumentIdsToModify.Count = 0 Then
+            MsgBox("Aucun document à modifier")
+            Exit Sub
+        End If
+
+        'Nombre de documents sélectionné
+        Dim NumberOfDocumentSelected As Integer
+        NumberOfDocumentSelected = DocumentIdsToModify.Count
+
+        'Récupération du nombre de propriété sélectionné
+        Dim NumberOfPropertySelected As Integer
+        NumberOfPropertySelected = GetCheckedItemCount(CheckedListBoxPropertyToExport)
+
+        ' Créer un tableau à deux dimensions (matrice)
+        Dim DocumentsPropertiesToExportArray(NumberOfDocumentSelected - 1, NumberOfPropertySelected - 1) As Object
+
+        ' Exemple : initialisation du tableau avec des valeurs
+        For documentIndex As Integer = 0 To NumberOfDocumentSelected - 1
+
+            Dim ActiveDocumentId As DocumentId = DocumentIdsToModify.Item(documentIndex)
+            Dim ActiveDocumentPdmObjectId As PdmObjectId = DocumentPdmIdsToModify.Item(documentIndex)
+
+            Dim ActiveColumn As Integer = 0
+
+            'Gestion Nom coché
+            If CheckedListBoxPropertyToExport.GetItemChecked(0) Then
+                DocumentsPropertiesToExportArray(documentIndex, ActiveColumn) = TopSolidHost.Pdm.GetName(ActiveDocumentPdmObjectId)
+                ActiveColumn = +1
+            End If
+
+            If CheckedListBoxPropertyToExport.GetItemChecked(1) Then
+                DocumentsPropertiesToExportArray(documentIndex, ActiveColumn) = TopSolidHost.Pdm.GetDescription(ActiveDocumentPdmObjectId)
+                ActiveColumn = +1
+            End If
+
+            If CheckedListBoxPropertyToExport.GetItemChecked(2) Then
+                DocumentsPropertiesToExportArray(documentIndex, ActiveColumn) = TopSolidHost.Pdm.GetPartNumber(ActiveDocumentPdmObjectId)
+                ActiveColumn = +1
+            End If
+
+
+            If CheckedListBoxPropertyToExport.GetItemChecked(3) Then
+                DocumentsPropertiesToExportArray(documentIndex, ActiveColumn) = TopSolidHost.Pdm.GetManufacturer(ActiveDocumentPdmObjectId)
+                ActiveColumn = +1
+            End If
+
+            If CheckedListBoxPropertyToExport.GetItemChecked(4) Then
+                DocumentsPropertiesToExportArray(documentIndex, ActiveColumn) = TopSolidHost.Pdm.GetManufacturerPartNumber(ActiveDocumentPdmObjectId)
+                ActiveColumn = +1
+            End If
+
+        Next
+
+
+        'Export du CSV
+
+        ' Chemin de destination et nom du fichier CSV
+        Dim csvFilePath As String = Path.Combine(SelectedCSVDestinationFolder, $"{CSVDocumentName}.csv")
+
+        ' Écrire les données dans le fichier CSV
+        Using writer As New StreamWriter(csvFilePath, False, System.Text.Encoding.UTF8)
+            For i As Integer = 0 To NumberOfDocumentSelected - 1
+                For j As Integer = 0 To NumberOfPropertySelected - 1
+                    ' Écrire la valeur dans le fichier avec le séparateur ;
+                    writer.Write(DocumentsPropertiesToExportArray(i, j))
+                    ' Ajouter le séparateur si ce n'est pas la dernière colonne
+                    If j < DocumentsPropertiesToExportArray.GetLength(1) - 1 Then
+                        writer.Write(";")
+                    End If
+                Next
+                ' Passer à la ligne suivante
+                writer.WriteLine()
+            Next
+        End Using
+
+        ' Ouvrir le document résultant
+        Process.Start(csvFilePath)
+
+    End Sub
+
+    Private Function GetCheckedItemCount(checkedListBox As CheckedListBox) As Integer
+        Dim count As Integer = 0
+
+        For Each itemIndex As Integer In checkedListBox.CheckedIndices
+            ' L'élément à l'index itemIndex est coché
+            count += 1
+        Next
+
+        ' Vous pouvez également utiliser la propriété CheckedItems pour obtenir les éléments cochés
+        ' For Each checkedItem As Object In checkedListBox.CheckedItems
+        '     count += 1
+        ' Next
+
+        Return count
+    End Function
+
+    Private Sub TextBoxCSVName_TextChanged(sender As Object, e As EventArgs) Handles TextBoxCSVName.TextChanged
+        CSVDocumentName = TextBoxCSVName.Text
+    End Sub
+End Class
